0176e98cb91944a94f2f01ae8caa1e98ca458ab3
Leon Schumacher <leonsch@protonmail.com>
2024-12-15 11:48:43 +0100

    vulkan/triangle-tutorial: create framebuffers

--- vulkan/triangle-tutorial/src/main.cpp
+++ vulkan/triangle-tutorial/src/main.cpp
@@ -221,24 +221,25 @@ class HelloTriangleApplication {
 	}
 
   private:
-	GLFWwindow*              window;
-	std::vector<const char*> validationLayers;
-	std::vector<const char*> extensions;
-	VkInstance               instance;
-	VkDebugUtilsMessengerEXT debugMessenger;
-	VkSurfaceKHR             surface;
-	VkPhysicalDevice         physicalDevice;
-	VkDevice                 device;
-	VkQueue                  graphicsQueue;
-	VkQueue                  presentQueue;
-	VkSwapchainKHR           swapChain;
-	std::vector<VkImage>     swapChainImages;
-	VkFormat                 swapChainImageFormat;
-	VkExtent2D               swapChainExtent;
-	std::vector<VkImageView> swapChainImageViews;
-	VkRenderPass             renderPass;
-	VkPipelineLayout         pipelineLayout;
-	VkPipeline               graphicsPipeline;
+	GLFWwindow*                window;
+	std::vector<const char*>   validationLayers;
+	std::vector<const char*>   extensions;
+	VkInstance                 instance;
+	VkDebugUtilsMessengerEXT   debugMessenger;
+	VkSurfaceKHR               surface;
+	VkPhysicalDevice           physicalDevice;
+	VkDevice                   device;
+	VkQueue                    graphicsQueue;
+	VkQueue                    presentQueue;
+	VkSwapchainKHR             swapChain;
+	std::vector<VkImage>       swapChainImages;
+	VkFormat                   swapChainImageFormat;
+	VkExtent2D                 swapChainExtent;
+	std::vector<VkImageView>   swapChainImageViews;
+	VkRenderPass               renderPass;
+	VkPipelineLayout           pipelineLayout;
+	VkPipeline                 graphicsPipeline;
+	std::vector<VkFramebuffer> swapChainFramebuffers;
 
 	const std::vector<const char*> deviceExtensions = {
 		VK_KHR_SWAPCHAIN_EXTENSION_NAME,
@@ -282,6 +283,7 @@ class HelloTriangleApplication {
 		createImageViews();
 		createRenderPass();
 		createGraphicsPipeline();
+		createFramebuffers();
 	}
 
 	void createInstance() {
@@ -830,6 +832,29 @@ class HelloTriangleApplication {
 		vkDestroyShaderModule(device, vertShaderModule, nullptr);
 	}
 
+	void createFramebuffers() {
+		swapChainFramebuffers.resize(swapChainImageViews.size());
+
+		for(size_t i = 0; i < swapChainImageViews.size(); i++) {
+			VkImageView attachments[] = {swapChainImageViews[i]};
+
+			VkFramebufferCreateInfo framebufferInfo{};
+			framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+			framebufferInfo.renderPass      = renderPass;
+			framebufferInfo.attachmentCount = ARRSIZE(attachments);
+			framebufferInfo.pAttachments    = attachments;
+			framebufferInfo.width           = swapChainExtent.width;
+			framebufferInfo.height          = swapChainExtent.height;
+			framebufferInfo.layers          = 1;
+
+			if(vkCreateFramebuffer(
+				   device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]
+			   ) != VK_SUCCESS) {
+				throw std::runtime_error("failed to create framebuffer!");
+			}
+		}
+	}
+
 	void mainLoop() {
 		// while(!glfwWindowShouldClose(window)) {
 		// 	glfwPollEvents();
@@ -837,6 +862,10 @@ class HelloTriangleApplication {
 	}
 
 	void cleanup() {
+		for(VkFramebuffer framebuffer : swapChainFramebuffers) {
+			vkDestroyFramebuffer(device, framebuffer, nullptr);
+		}
+
 		vkDestroyPipeline(device, graphicsPipeline, nullptr);
 		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
 		vkDestroyRenderPass(device, renderPass, nullptr);
