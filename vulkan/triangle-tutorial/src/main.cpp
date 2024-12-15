78415819d696f4336739b713a5a95816b079439f
Leon Schumacher <leonsch@protonmail.com>
2024-12-15 10:04:06 +0100

    vulkan/triangle-tutorial: create fixed pipeline functions

--- vulkan/triangle-tutorial/src/main.cpp
+++ vulkan/triangle-tutorial/src/main.cpp
@@ -234,6 +234,7 @@ class HelloTriangleApplication {
 	VkFormat                 swapChainImageFormat;
 	VkExtent2D               swapChainExtent;
 	std::vector<VkImageView> swapChainImageViews;
+	VkPipelineLayout         pipelineLayout;
 
 	const std::vector<const char*> deviceExtensions = {
 		VK_KHR_SWAPCHAIN_EXTENSION_NAME,
@@ -694,6 +695,77 @@ class HelloTriangleApplication {
 			fragShaderStageInfo,
 		};
 
+		VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
+		vertexInputInfo.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+		vertexInputInfo.vertexBindingDescriptionCount   = 0;
+		vertexInputInfo.vertexAttributeDescriptionCount = 0;
+
+		VkPipelineInputAssemblyStateCreateInfo inputAssembly = {};
+		inputAssembly.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+		inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+		inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+		std::vector<VkDynamicState> dynamicStates = {
+			VK_DYNAMIC_STATE_VIEWPORT,
+			VK_DYNAMIC_STATE_SCISSOR,
+		};
+
+		VkPipelineDynamicStateCreateInfo dynamicState = {};
+		dynamicState.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
+		dynamicState.dynamicStateCount = dynamicStates.size();
+		dynamicState.pDynamicStates    = dynamicStates.data();
+
+		VkPipelineViewportStateCreateInfo viewportState = {};
+		viewportState.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+		viewportState.viewportCount = 1;
+		viewportState.scissorCount  = 1;
+
+		VkPipelineRasterizationStateCreateInfo rasterizer = {};
+		rasterizer.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+		rasterizer.depthClampEnable        = VK_FALSE;
+		rasterizer.rasterizerDiscardEnable = VK_FALSE;
+		rasterizer.polygonMode             = VK_POLYGON_MODE_FILL;
+		rasterizer.lineWidth               = 1.0f;
+		rasterizer.cullMode                = VK_CULL_MODE_BACK_BIT;
+		rasterizer.frontFace               = VK_FRONT_FACE_CLOCKWISE;
+		rasterizer.depthBiasEnable         = VK_FALSE;
+
+		VkPipelineMultisampleStateCreateInfo multisampling = {};
+		multisampling.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+		multisampling.sampleShadingEnable  = VK_FALSE;
+		multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+		VkPipelineColorBlendAttachmentState colorBlendAttachment = {};
+		colorBlendAttachment.colorWriteMask                      = //
+			VK_COLOR_COMPONENT_R_BIT |                             //
+			VK_COLOR_COMPONENT_G_BIT |                             //
+			VK_COLOR_COMPONENT_B_BIT |                             //
+			VK_COLOR_COMPONENT_A_BIT;                              //
+		colorBlendAttachment.blendEnable = VK_FALSE;
+
+		VkPipelineColorBlendStateCreateInfo colorBlending = {};
+		colorBlending.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+		colorBlending.logicOpEnable   = VK_FALSE;
+		colorBlending.attachmentCount = 1;
+		colorBlending.pAttachments    = &colorBlendAttachment;
+
+		VkPipelineLayoutCreateInfo pipelineLayoutInfo = {};
+		pipelineLayoutInfo.sType =
+			VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+
+		if(vkCreatePipelineLayout(
+			   device, &pipelineLayoutInfo, nullptr, &pipelineLayout
+		   ) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create pipeline layout!");
+		}
+
 		vkDestroyShaderModule(device, fragShaderModule, nullptr);
 		vkDestroyShaderModule(device, vertShaderModule, nullptr);
 	}
@@ -705,6 +777,8 @@ class HelloTriangleApplication {
 	}
 
 	void cleanup() {
+		vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+
 		for(VkImageView imageView : swapChainImageViews) {
 			vkDestroyImageView(device, imageView, nullptr);
 		}
